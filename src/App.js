e7a0390bc9b2586cdbb5241dec5c5e62b876f77f
mcdelacruz8 <88753390+mcdelacruz8@users.noreply.github.com>
2022-08-09 16:49:19 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="
+      abc95c2e-22b2-4c14-b662-c9e93a5877fd"
+      userName="mdlc"
+      userSecret="123123"
+    />
+  );
+}
+
+export default App;
