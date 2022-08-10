14027df3a432b2f6516470d48da2328aea429381
mcdelacruz8 <88753390+mcdelacruz8@users.noreply.github.com>
2022-08-10 14:21:06 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import LoginForm from './components/LoginForm';
+import ChatFeed from './components/ChatFeed';
+
 import './App.css';
 
 const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />
+
   return (
     <ChatEngine
       height="100vh"
       projectID="
       abc95c2e-22b2-4c14-b662-c9e93a5877fd"
-      userName="mdlc"
-      userSecret="123123"
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
 }
